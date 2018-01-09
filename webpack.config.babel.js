fdce6af1292d0f5a53a79ad50fab78eb5281c62f
azenakhi <ahmed.zenakhi@gmail.com>
2018-01-09 14:53:22 +0100

    Update webpack.config.babel.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,14 +4,16 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 
 export default () => ({
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
         rules: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=100000' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
             { test: /\.html$/, loader: 'html-loader' },
             { test: /\.(jpg)$/, loader: 'file-loader?name=[name].[ext]&outputPath=assets/' }
         ]
